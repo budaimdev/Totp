e5df096e35ffab4f1e2c9f19219b222aadd84f07
budaimdev <michalBudai@outlook.cz>
2026-05-23 01:10:09 +0200

    Added basic navigation and pages

---
+++ lib/screens/add.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Add extends StatelessWidget {
+  const Add({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("Add screen"),
+    );
+  }
+
+}
